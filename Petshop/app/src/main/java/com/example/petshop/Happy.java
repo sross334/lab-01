6a027139663451b44399ab2dd2f037a3178da175
Sean Ross <seanashross@outlook.com>
2026-01-08 14:56:01 -0700

    updated readme,license, and added code

---
+++ Petshop/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood{
+    public Happy(Date date) {
+        super(date);
+    }
+
+    public Happy(){
+        super();
+    }
+
+    @Override
+    public String emote(){
+        return "yay";
+    }
+}
---
+++ Petshop/app/src/main/java/com/example/petshop/Mood.java
@@ -0,0 +1,23 @@
+package com.example.petshop;
+import java.util.Date;
+public abstract class Mood {
+
+    private Date date;
+    public Mood(Date date){
+        this.date = date;
+    }
+
+    public Mood(){
+        this.date = new Date();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public abstract String emote();
+}
